98c71faf2a98630b69fddbc23a11cf4248a3b501
Dominic Cerquetti <binary1230@users.noreply.github.com>
2021-12-22 20:39:40 -0500

    fix build.bat exe path

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo "building..."
 
 REM start.asm is hand-generated and includes both 1) the original unmodified game and 2) any custom changes
-bin/asar.exe -v start.asm output.sfc
+.\bin\asar-domfix-1.81.exe -v start.asm output.sfc
 
 @echo "output written to output.sfc"
 pause
